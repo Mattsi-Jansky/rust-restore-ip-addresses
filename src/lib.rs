12d467251d0edaf960c5fb1c7e5a7f6dc2acbbf5
MiyamotoAkira <yefo.akira@twoormore.eu>
2022-09-24 15:46:18 +0100

    Possible setup for the branching strategy

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub struct Solution { }
+pub struct Solution {}
 
 impl Solution {
     pub fn restore_ip_addresses(s: String) -> Vec<String> {
@@ -13,6 +13,14 @@ impl Solution {
 
         vec![result]
     }
+
+    pub fn get_next_generation(
+        s: String,
+        current_index: usize,
+        branch_state: String,
+    ) -> (Vec<String>, usize) {
+        (vec![String::new()], current_index)
+    }
 }
 
 #[cfg(test)]
@@ -24,19 +32,20 @@ mod tests {
     fn acceptance_one() {
         let result = Solution::restore_ip_addresses(String::from("25525511135"));
 
-        assert_eq!(result, vec![
-            String::from("255.255.11.135"),
-            String::from("255.255.111.35")
-        ])
+        assert_eq!(
+            result,
+            vec![
+                String::from("255.255.11.135"),
+                String::from("255.255.111.35")
+            ]
+        )
     }
 
     #[test]
     fn acceptance_two() {
         let result = Solution::restore_ip_addresses(String::from("0000"));
 
-        assert_eq!(result, vec![
-            String::from("0.0.0.0")
-        ])
+        assert_eq!(result, vec![String::from("0.0.0.0")])
     }
 
     #[test]
@@ -44,21 +53,61 @@ mod tests {
     fn acceptance_three() {
         let result = Solution::restore_ip_addresses(String::from("101023"));
 
-        assert_eq!(result, vec![
-            String::from("1.0.10.23"),
-            String::from("1.0.102.3"),
-            String::from("10.1.0.23"),
-            String::from("10.10.2.3"),
-            String::from("101.0.2.3"),
-        ])
+        assert_eq!(
+            result,
+            vec![
+                String::from("1.0.10.23"),
+                String::from("1.0.102.3"),
+                String::from("10.1.0.23"),
+                String::from("10.10.2.3"),
+                String::from("101.0.2.3"),
+            ]
+        )
     }
+
+    // #[test]
+    // fn five_characters() {
+    //     let result = Solution::restore_ip_addresses(String::from("10000"));
+
+    //     assert_eq!(result, vec![
+    //         String::from("10.0.0.0")
+    //     ])
+    // }
+
+    // #[test]
+    // fn get_all_branches() {
+    //     let result = Solution::restore_ip_addresses(String::from("11111"));
+
+    //     assert_eq!(
+    //         result,
+    //         vec![
+    //             String::from("11.1.1.1"),
+    //             String::from("1.11.1.1"),
+    //             String::from("1.1.11.1"),
+    //             String::from("1.1.1.11"),
+    //         ]
+    //     )
+    // }
 
     #[test]
-    fn five_characters() {
-        let result = Solution::restore_ip_addresses(String::from("10000"));
+    fn branch_single_character_generation() {
+        let (result, index) = Solution::get_next_generation(String::from("1"), 0, String::from(""));
 
-        assert_eq!(result, vec![
-            String::from("10.0.0.0")
-        ])
+        assert_eq!(result, vec![String::from("1")]);
+        assert_eq!(index, 1);
     }
+
+    // #[test]
+    // fn branch_single_character_second_generation() {
+    //     let result = get_next_generation(String::from("1"), 1, String::from("1"));
+
+    //     assert_eq!(result, vec![]);
+    // }
+
+    // #[test]
+    // fn branch_two_characters() {
+    //     let (result, index) = Solution::get_next_generation(String::from("11"), 0, String::from(""));
+
+    //     assert_eq!(result, vec![String::from("11"), String::from("1.1")]);
+    // }
 }
